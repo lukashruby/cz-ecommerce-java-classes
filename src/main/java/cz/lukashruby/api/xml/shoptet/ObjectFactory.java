708d923c8e9c258565a954f2484ef930fdff3ea5
Lukas Hruby (V) <lukas.hruby@gmail.com>
2016-03-22 15:48:43 +0100

    clean code

--- src/main/java/cz/lukashruby/api/xml/shoptet/ObjectFactory.java
+++ src/main/java/cz/lukashruby/api/xml/shoptet/ObjectFactory.java
@@ -4,139 +4,79 @@ package cz.lukashruby.api.xml.shoptet;
 import javax.xml.bind.annotation.XmlRegistry;
 
 
-/**
- * This object contains factory methods for each 
- * Java content interface and Java element interface 
- * generated in the cz.lukashruby.dopo.onlinekoupelnyFeeder.xml.classes.shoptet package. 
- * <p>An ObjectFactory allows you to programatically 
- * construct new instances of the Java representation 
- * for XML content. The Java representation of XML 
- * content can consist of schema derived interfaces 
- * and classes representing the binding of schema 
- * type definitions, element declarations and model 
- * groups.  Factory methods for each of these are 
- * provided in this class.
- * 
- */
 @XmlRegistry
 public class ObjectFactory {
 
 
-    /**
-     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: cz.lukashruby.dopo.onlinekoupelnyFeeder.xml.classes.shoptet
-     * 
-     */
     public ObjectFactory() {
     }
 
-    /**
-     * Create an instance of {@link SHOP }
-     * 
-     */
+
     public SHOP createSHOP() {
         return new SHOP();
     }
 
-    /**
-     * Create an instance of {@link SHOP.SHOPITEM }
-     * 
-     */
+
     public SHOP.SHOPITEM createSHOPSHOPITEM() {
         return new SHOP.SHOPITEM();
     }
 
-    /**
-     * Create an instance of {@link SHOP.SHOPITEM.VARIANTS }
-     * 
-     */
+
     public SHOP.SHOPITEM.VARIANTS createSHOPSHOPITEMVARIANTS() {
         return new SHOP.SHOPITEM.VARIANTS();
     }
 
-    /**
-     * Create an instance of {@link SHOP.SHOPITEM.VARIANTS.VARIANT }
-     * 
-     */
+
     public SHOP.SHOPITEM.VARIANTS.VARIANT createSHOPSHOPITEMVARIANTSVARIANT() {
         return new SHOP.SHOPITEM.VARIANTS.VARIANT();
     }
 
-    /**
-     * Create an instance of {@link SHOP.SHOPITEM.VARIANTS.VARIANT.PARAMETERS }
-     * 
-     */
+
     public SHOP.SHOPITEM.VARIANTS.VARIANT.PARAMETERS createSHOPSHOPITEMVARIANTSVARIANTPARAMETERS() {
         return new SHOP.SHOPITEM.VARIANTS.VARIANT.PARAMETERS();
     }
 
-    /**
-     * Create an instance of {@link SHOP.SHOPITEM.TEXTPROPERTIES }
-     * 
-     */
+
     public SHOP.SHOPITEM.TEXTPROPERTIES createSHOPSHOPITEMTEXTPROPERTIES() {
         return new SHOP.SHOPITEM.TEXTPROPERTIES();
     }
 
-    /**
-     * Create an instance of {@link SHOP.SHOPITEM.CATEGORIES }
-     * 
-     */
+
     public SHOP.SHOPITEM.CATEGORIES createSHOPSHOPITEMCATEGORIES() {
         return new SHOP.SHOPITEM.CATEGORIES();
     }
 
-    /**
-     * Create an instance of {@link SHOP.SHOPITEM.IMAGES }
-     * 
-     */
+
     public SHOP.SHOPITEM.IMAGES createSHOPSHOPITEMIMAGES() {
         return new SHOP.SHOPITEM.IMAGES();
     }
 
-    /**
-     * Create an instance of {@link SHOP.SHOPITEM.RELATEDPRODUCTS }
-     * 
-     */
+
     public SHOP.SHOPITEM.RELATEDPRODUCTS createSHOPSHOPITEMRELATEDPRODUCTS() {
         return new SHOP.SHOPITEM.RELATEDPRODUCTS();
     }
 
-    /**
-     * Create an instance of {@link SHOP.SHOPITEM.ALTERNATIVEPRODUCTS }
-     * 
-     */
+
     public SHOP.SHOPITEM.ALTERNATIVEPRODUCTS createSHOPSHOPITEMALTERNATIVEPRODUCTS() {
         return new SHOP.SHOPITEM.ALTERNATIVEPRODUCTS();
     }
 
-    /**
-     * Create an instance of {@link SHOP.SHOPITEM.FLAGS }
-     * 
-     */
+
     public SHOP.SHOPITEM.FLAGS createSHOPSHOPITEMFLAGS() {
         return new SHOP.SHOPITEM.FLAGS();
     }
 
-    /**
-     * Create an instance of {@link SHOP.SHOPITEM.STOCK }
-     * 
-     */
+
     public SHOP.SHOPITEM.STOCK createSHOPSHOPITEMSTOCK() {
         return new SHOP.SHOPITEM.STOCK();
     }
 
-    /**
-     * Create an instance of {@link SHOP.SHOPITEM.VARIANTS.VARIANT.PARAMETERS.PARAMETER }
-     * 
-     */
+
     public SHOP.SHOPITEM.VARIANTS.VARIANT.PARAMETERS.PARAMETER createSHOPSHOPITEMVARIANTSVARIANTPARAMETERSPARAMETER() {
         return new SHOP.SHOPITEM.VARIANTS.VARIANT.PARAMETERS.PARAMETER();
     }
 
-    /**
-     * Create an instance of {@link SHOP.SHOPITEM.TEXTPROPERTIES.TEXTPROPERTY }
-     * 
-     */
+
     public SHOP.SHOPITEM.TEXTPROPERTIES.TEXTPROPERTY createSHOPSHOPITEMTEXTPROPERTIESTEXTPROPERTY() {
         return new SHOP.SHOPITEM.TEXTPROPERTIES.TEXTPROPERTY();
     }
--- src/main/java/cz/lukashruby/api/xml/shoptet/SHOP.java
+++ src/main/java/cz/lukashruby/api/xml/shoptet/SHOP.java
@@ -442,23 +442,6 @@ public class SHOP {
         }
 
 
-        /**
-         * <p>Java class for anonymous complex type.
-         * <p>
-         * <p>The following schema fragment specifies the expected content contained within this class.
-         * <p>
-         * <pre>
-         * &lt;complexType>
-         *   &lt;complexContent>
-         *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
-         *       &lt;sequence>
-         *         &lt;element name="CODE" type="{http://www.w3.org/2001/XMLSchema}string" maxOccurs="unbounded" minOccurs="0"/>
-         *       &lt;/sequence>
-         *     &lt;/restriction>
-         *   &lt;/complexContent>
-         * &lt;/complexType>
-         * </pre>
-         */
         @XmlAccessorType(XmlAccessType.FIELD)
         @XmlType(name = "", propOrder = {
                 "code"
