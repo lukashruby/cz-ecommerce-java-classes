fff26260cbde77d91bd7f3fb3689432029881717
Lukas Hruby <lukas.hruby@gmail.com>
2017-04-17 21:22:28 +0200

    update rappa item - add tostring and lombok

--- src/main/java/cz/lukashruby/api/xml/rappa/Item.java
+++ src/main/java/cz/lukashruby/api/xml/rappa/Item.java
@@ -1,5 +1,8 @@
 package cz.lukashruby.api.xml.rappa;
 
+import lombok.Data;
+import lombok.ToString;
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
@@ -33,6 +36,8 @@ import java.util.List;
         "discountgroup",
         "colors"
 })
+@Data
+@ToString
 public class Item {
 
     @XmlElement(name = "PRODUCT", required = true)
@@ -80,85 +85,6 @@ public class Item {
     @XmlElement(name = "COLORS", required = true)
     protected String colors;
 
-    public String getPRODUCT() {
-        return product;
-    }
-
-    public void setPRODUCT(String value) {
-        this.product = value;
-    }
-
-    public String getDESCRIPTION() {
-        return description;
-    }
-
-    public void setDESCRIPTION(String value) {
-        this.description = value;
-    }
-
-    public String getURL() {
-        return url;
-    }
-
-    public void setURL(String value) {
-        this.url = value;
-    }
-
-    public String getDELIVERYDATE() {
-        return deliverydate;
-    }
-
-    public void setDELIVERYDATE(String value) {
-        this.deliverydate = value;
-    }
-
-    public String getKOD() {
-        return kod;
-    }
-
-    public void setKOD(String value) {
-        this.kod = value;
-    }
-
-    public BigDecimal getPRICE() {
-        return price;
-    }
-
-    public void setPRICE(BigDecimal value) {
-        this.price = value;
-    }
-
-    public BigDecimal getPRICEVAT() {
-        return pricevat;
-    }
-
-    public void setPRICEVAT(BigDecimal value) {
-        this.pricevat = value;
-    }
-
-    public BigDecimal getPRICEVO() {
-        return pricevo;
-    }
-
-    public void setPRICEVO(BigDecimal value) {
-        this.pricevo = value;
-    }
-
-    public BigDecimal getPRICEVOVAT() {
-        return pricevovat;
-    }
-
-    public void setPRICEVOVAT(BigDecimal value) {
-        this.pricevovat = value;
-    }
-
-    public String getIMGURL() {
-        return imgurl;
-    }
-
-    public void setIMGURL(String value) {
-        this.imgurl = value;
-    }
 
     public List<String> getCATEGORYTEXT() {
         if (categorytext == null) {
@@ -167,92 +93,8 @@ public class Item {
         return this.categorytext;
     }
 
-    public String getMANUFACTURER() {
-        return manufacturer;
+    private void setCATEGORYTEXT(List<String> categorytext) {
     }
 
-    public void setMANUFACTURER(String value) {
-        this.manufacturer = value;
-    }
-
-    public Images getIMAGES() {
-        return images;
-    }
-
-    public void setIMAGES(Images value) {
-        this.images = value;
-    }
-
-    public String getEAN() {
-        return ean;
-    }
-
-    public void setEAN(String value) {
-        this.ean = value;
-    }
-
-    public String getPACKAGEMINIMUM() {
-        return packageminimum;
-    }
-
-    public void setPACKAGEMINIMUM(String value) {
-        this.packageminimum = value;
-    }
-
-    public String getPACKAGESIZE() {
-        return packagesize;
-    }
-
-    public void setPACKAGESIZE(String value) {
-        this.packagesize = value;
-    }
-
-    public String getPACKAGING() {
-        return packaging;
-    }
-
-    public void setPACKAGING(String value) {
-        this.packaging = value;
-    }
-
-    public String getSTOCK() {
-        return stock;
-    }
-
-    public void setSTOCK(String value) {
-        this.stock = value;
-    }
-
-    public String getAGE() {
-        return age;
-    }
-
-    public void setAGE(String value) {
-        this.age = value;
-    }
-
-    public String getDISCOUNTGROUPID() {
-        return discountgroupid;
-    }
-
-    public void setDISCOUNTGROUPID(String value) {
-        this.discountgroupid = value;
-    }
-
-    public String getDISCOUNTGROUP() {
-        return discountgroup;
-    }
-
-    public void setDISCOUNTGROUP(String value) {
-        this.discountgroup = value;
-    }
-
-    public String getCOLORS() {
-        return colors;
-    }
-
-    public void setCOLORS(String value) {
-        this.colors = value;
-    }
 
 }
